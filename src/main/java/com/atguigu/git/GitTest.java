c3428ea0f24ab43d0abf5f91f75934c88b199a2d
Nxx19 <105934001+Nxx19@users.noreply.github.com>
2023-06-22 13:45:24 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.atguigu.git;
+
+/**
+ * @Author nfx
+ * @Create 2023/6/22
+ * Description:
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello git");
+
+    }
+
+}
